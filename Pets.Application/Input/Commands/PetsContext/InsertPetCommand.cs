94e9ad90e9fa1f98f3b45dd136a8d8b89f7fe470
Diego Magalhães <diego150691@gmail.com>
2021-07-16 20:36:58 -0300

    Criacao command pet

---
+++ Pets.Application/Input/Commands/PetsContext/InsertPetCommand.cs
@@ -0,0 +1,9 @@
+using Pets.Application.Input.Commands.Interfaces;
+
+namespace Pets.Application.Input.Commands.PetsContext
+{
+    public class InsertPetCommand : ICommandBase
+    {
+        
+    }
+}
